7b6a5e8cf8993a86b1fa85921e98eb85f11538f2
Costyan <nemchenkokostya@gmail.com>
2015-06-08 23:00:05 +0300

    2015.06.08 v1.2

--- frontend/views/news/index.php
+++ frontend/views/news/index.php
@@ -1,9 +1,13 @@
 <?php
 use yii\helpers\Html;
+
+$this->title = 'News';
 ?>
 <center><h1>News</h1></center>
 <?php foreach ($news as $myNews):?>
-<?= Html::encode("{$myNews->TitleNews}") ?>
+<center><h3><?= Html::encode("{$myNews->TitleNews}") ?></h3></center>
+<br>
+<?= Html::encode("{$myNews->DescriptionNews}") ?>
 <?php endforeach; ?>
 
 
--- frontend/views/news2/index.php
+++ frontend/views/news2/index.php
@@ -27,7 +27,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'DescriptionNews',
             'ContentsNews',
             'DateNews',
-            // 'Author',
+            'Author',
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
